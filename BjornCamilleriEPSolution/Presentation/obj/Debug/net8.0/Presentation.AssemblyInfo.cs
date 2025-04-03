e8cf9a15dd384d6d5edd19d09518908597026a22
BjornCamilleri <bjorn.camilleri.g53384@mcast.edu.mt>
2025-04-03 22:29:36 +0200

    Reinstalled NuGet Packages with correct versions

--- BjornCamilleriEPSolution/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ BjornCamilleriEPSolution/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00df378de1083137db7eb8497e5bbdc9360857a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1692afbf4290c21472e0b18b8e6698ee6ec3bd67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
