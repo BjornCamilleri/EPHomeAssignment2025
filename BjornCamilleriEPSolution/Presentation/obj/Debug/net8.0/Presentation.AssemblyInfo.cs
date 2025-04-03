1692afbf4290c21472e0b18b8e6698ee6ec3bd67
BjornCamilleri <bjorn.camilleri.g53384@mcast.edu.mt>
2025-04-03 21:25:49 +0200

    Installed Nuget packages

--- BjornCamilleriEPSolution/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
+++ BjornCamilleriEPSolution/Presentation/obj/Debug/net8.0/Presentation.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c00df378de1083137db7eb8497e5bbdc9360857a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
